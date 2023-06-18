4701fc410f99b8c3c6c348798a59e2143c585fdf
NamanK24 <rsnaman24@gmail.com>
2023-06-18 22:46:19 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+
+import plugin from 'tailwindcss/plugin';
+
+const rotateY = plugin(function ({ addUtilities }) {
+  addUtilities ({
+    '.rotate-y-180': {
+      transform: "rotateY(180deg)"
+    },
+    '.-rotate-y-180': {
+      transform: "rotateY(-180deg)"
+    }
+  })
+})
+
+export const content = ["./*.{html,js}"];
+export const theme = {
+  extend: {
+    colors: {
+      "color-primary": "#01051e",
+      "color-primary-light": "#020726",
+      "color-primary-dark": "#010417",
+      "color-secondary": "#ff7d3b",
+      "color-gray": "#333",
+      "color-white": "#fff",
+      "color-blob": "#A427DF",
+    }
+  },
+  container: {
+    center: true,
+    padding: {
+      DEFAULT: '20px',
+      md: "50px"
+    }
+  }
+};
+export const plugins = [rotateY];
